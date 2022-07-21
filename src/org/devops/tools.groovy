11f0a0a84cb37081611601b888579426e2f0762c
jxsrzyc <53077804+jxsrzyc@users.noreply.github.com>
2022-07-21 22:35:55 +0800

    增加输出字体颜色方法

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -1,10 +1,5 @@
 package org.devops
 
-//打印内容
-def PrintMes(content){
-    println(content)
-}
-
 //格式化输出
 def PrintMes(value,color){
     colors = ['red'   : "\033[40;31m >>>>>>>>>>>${value}<<<<<<<<<<< \033[0m",
